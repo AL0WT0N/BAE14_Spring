0abacc79282e72d48c81e68bf4a16e3725f86c70
ALowton <ALowton@QA.com>
2022-04-20 12:48:00 +0100

    AL: added a getByID request handler

--- src/main/java/com/qa/baespring/controller/UserController.java
+++ src/main/java/com/qa/baespring/controller/UserController.java
@@ -1,16 +1,28 @@
 package com.qa.baespring.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.baespring.domain.User;
+import com.qa.baespring.service.UserService;
 
 @RestController
 public class UserController {
 
-	// Get
-	@GetMapping("/hello") // localhost:8080/hello
-	public String helloWorld() {
-		return "Hello world";
+	private UserService service;
+	
+	public UserController(UserService service) {
+		this.service = service;
 	}
+
+	// Get by ID (get one User)
+	@GetMapping("/getById/{id}") // localhost:8080/getById/id
+	public User getById(@PathVariable long id) {
+		return service.getById(id);
+	}
+	
+//	@GetMapping("/getAll") // localhost:8080/getAll
 	
 	// Post
 	
--- src/main/java/com/qa/baespring/service/UserService.java
+++ src/main/java/com/qa/baespring/service/UserService.java
@@ -1,7 +1,9 @@
 package com.qa.baespring.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import com.qa.baespring.domain.User;
 import com.qa.baespring.repo.UserRepo;
 
 @Service
@@ -13,6 +15,10 @@ public class UserService {
 		this.repo = repo;
 	}
 	
+	// Get By ID (get one User)
+	public User getById(long id) {
+		return repo.findById(id).get(); //.get() will either get the User (if exists) OR throw NoSuchElementException
+	}
 	
 	
 }
