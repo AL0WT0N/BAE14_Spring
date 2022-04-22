b66e296b38b67ea7fc4f14faeef0a6a72711071e
ALowton <ALowton@QA.com>
2022-04-22 12:02:53 +0100

    AL: added sql annotation

--- src/test/java/com/qa/baespring/controller/UserControllerIntegrationTest.java
+++ src/test/java/com/qa/baespring/controller/UserControllerIntegrationTest.java
@@ -3,10 +3,15 @@ package com.qa.baespring.controller;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Sql(scripts = {"classpath:testschema.sql", "classpath:testdata.sql"}, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
 @ActiveProfiles("test")
 public class UserControllerIntegrationTest {
 
+	
+	
 }
