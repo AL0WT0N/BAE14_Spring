f77b14269930614d1fb6241bbb7ceebe41f36423
ALowton <ALowton@QA.com>
2022-04-22 13:03:17 +0100

    AL: added create test to controller integration test

--- src/test/java/com/qa/baespring/controller/UserControllerIntegrationTest.java
+++ src/test/java/com/qa/baespring/controller/UserControllerIntegrationTest.java
@@ -1,10 +1,21 @@
 package com.qa.baespring.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.qa.baespring.domain.User;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -12,6 +23,25 @@ import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 @ActiveProfiles("test")
 public class UserControllerIntegrationTest {
 
+	@Autowired
+	private MockMvc mvc;
 	
+	@Autowired
+	private ObjectMapper mapper;
 	
+	@Test
+	public void createTest() throws Exception {
+		User entry = new User("Jim", "Jones", "jjones1");
+		String entryAsJSON = mapper.writeValueAsString(entry);
+		
+		User result = new User(2L, "Jim", "Jones", "jjones1");
+		String resultAsJSON = mapper.writeValueAsString(result);
+		
+		mvc.perform(post("/user/create")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(entryAsJSON))
+				.andExpect(status().isCreated())
+				.andExpect(content().json(resultAsJSON));
+		
+	}
 }
